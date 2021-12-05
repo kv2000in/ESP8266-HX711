4a0e737ea4d32d67db3cdc1362413b7c869f6829
kv2000in <kv2000inn@gmail.com>
2021-12-05 16:28:18 -0500

    Major changes 1) ESP Server added which runs in AP mode, has a file system to store data and also serves web pages (to be implemented) to connecting users. 2) ESP-HX711 nodes connect to the server, transmit the data and go back to sleep q 30 secs To do: Server web interface - attach the nodes to their function, tare, calibrate, save calibration data, start saving node data, serve real time node data

--- ESP-HX711/ESP-HX711.ino
+++ ESP-HX711/ESP-HX711.ino
@@ -16,23 +16,23 @@ Adjust the parameter in step 4 until you get an accurate reading.
 
 #include "HX711.h"
 #include <ESP8266WiFi.h>
-#include <WebSocketsServer.h>
-#include <ESP8266WebServer.h>
+
 #include <string.h>
-#include <ArduinoOTA.h>
-#include <ESP8266HTTPUpdateServer.h>
 
-#ifndef APSSID
-#define APSSID "myScale1"
-#define APPSK  "elacSym123"
-#endif
+WiFiClient client;
 
-const char *ssid = APSSID;
-const char *password = APPSK;
+// WiFi credentials.
+const char* WIFI_SSID = "ESP";
+const char* WIFI_PASS = "monte123";
+const char* host = "192.168.4.1";  // TCP Server IP
+const int   port = 9999;            // TCP Server Port
+
+int batteryVoltage;   
+int R1=22;//22k
+int R2=10;//10k
+
 String mystring;
-ESP8266WebServer server(80);
-WebSocketsServer webSocket = WebSocketsServer(8000);
-ESP8266HTTPUpdateServer httpUpdater;
+
 
 // HX711 circuit wiring
 const int LOADCELL_DOUT_PIN = 4; //Hardwired
@@ -48,303 +48,75 @@ HX711 scale;
 
 
 
+void connect() {
 
 
+WiFi.mode(WIFI_STA);
+WiFi.begin(WIFI_SSID, WIFI_PASS);
+//WiFi.config(IPAddress(192,168,1,204), IPAddress(192,168,1,2), IPAddress(255,255,255,0),IPAddress(192,168,1,2));
+unsigned long wifiConnectStart = millis();
 
-
-
-
-/* Set these to your desired credentials. */
-
-
-static const char PROGMEM MANIFEST_JSON[] = R"rawliteral(
-{
-"name": "myROVER",
-"short_name": "myrover",
-"description": "A simple rover controller app.",
-"display": "standalone",
-"scope": "/",
-"orientation":  "landscape"
-}
-)rawliteral";
-
-static const char PROGMEM CHECK_SENSORS[] = R"rawliteral(<!doctype html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-</head>
-<body>
-</body>
-</html>
-)rawliteral";
-static const char PROGMEM INDEX_HTML[] = R"rawliteral(
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
-      <link rel="manifest" href="manifest.json">
-        <style>
-          /*        disable scrolling    */
-          html {
-            margin: 0;
-            padding: 0;
-            overflow: hidden;
-          }
-        
-        body {
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          overflow: auto;
-          margin: 0px;
-          
-        }
-        
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-        }
-        
-        
-        @media only screen and (max-height: 444px) {
-          .horizontalslider {
-            position: relative;
-            height: 10%;
-          }
-        }
-        
-                  
-        
-                    .mybuttons {
-  display: inline-block;
-  padding: 15px 25px;
-  font-size: 24px;
-  cursor: pointer;
-  text-align: center;
-  text-decoration: none;
-  outline: none;
-  color: #fff;
-  background-color: #4CAF50;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 0 9px #999;
+while (WiFi.status() != WL_CONNECTED) {
+// Check to see if
+if (WiFi.status() == WL_CONNECT_FAILED) {
+Serial.println("WiFi Connection Failed");
+delay(10000);
 }
 
-.mybuttons:hover {background-color: #3e8e41}
+delay(500);
 
-.mybuttons:active {
-  background-color: #3e8e41;
-  box-shadow: 0 5px #666;
-  transform: translateY(4px);
+// Only try for 5 seconds.
+if (millis() - wifiConnectStart > 15000) {
+Serial.println("WiFi Stopped Trying");
+return;
 }
-        .col-1 {
-          position: absolute;
-          width: 24%;
-          right: 75%
-        }
-        
-        .col-2 {
-          position: absolute;
-          width: 46%;
-          right: 38%;
-        }
-        
-        .col-3 {
-          position: absolute;
-          width: 38%;
-          right: 0;
-        }
-        #websocketstatus[type="radio"]:disabled {
-          
-          color: aqua;
-          background-color: aqua;
-          border-color: aquamarine;
-          
-          
-        }
-        </style>
-        <title>myScale1</title>
-        </head>
-  <body>
-        <div class = "col-1">
-    <label for="websocketstatus">Connected</label>
-            <input type="radio" id="websocketstatus" disabled>
-        <br><br><br>
-        <button class="mybuttons" id="connectWS" onclick='doConnect()'>Connect
-        </button>
-            <br><br><br>
-                    <button class="mybuttons" id="closeWS" onclick='doClose()'>Close
-        </button>
-            <br><br><br>    
-            <button class="mybuttons" id="reloadfromsource" onclick='location.reload()'>Reload
-        </button>
-            <br><br><br>    
-            <button class="mybuttons" id="tareScale" onclick='tareScale()'>Tare
-        </button>
-             <br><br><br>    
-            <button class="mybuttons" id="calibrateScale" onclick='calibrateScale()'>Calibrate
-        </button>
-            </div>
-        <button class = "col-3 mybuttons" id ="serialConsole"></button>
-  </body>
-  <script>
-    
-    
-  function tareScale(){
-        doSend("tare");
-    }
-  function handlewebsocketstatus(){
-    if (iswebsocketconnected){
-      document.getElementById("websocketstatus").checked=true;
-    }
-    else
-    {
-      document.getElementById("websocketstatus").checked=false;
-    }
-  }       
-  
-  
-  var websock;
-    var calibrationFactor = 1;
-    function calibrateScale(){
-        calibrationFactor= prompt("Calibration Factor", calibrationFactor);
-    }
-  var iswebsocketconnected = false;
-  function doConnect() {
-      // websock = new WebSocket('ws://' + window.location.hostname + ':8000/');
-      websock = new WebSocket('ws://192.168.4.1:8000/');
-    
-      websock.onopen = function(evt) {
-        console.log('websock open');
-        iswebsocketconnected = true;
-        handlewebsocketstatus();
-        
-      }
-      
-      websock.onclose = function(evt) {
-        iswebsocketconnected = false;
-        console.log('websock close');
-        handlewebsocketstatus();
-          //clear all the intervals - doesn't work. still dealing with "ghost" setIntervals
-          
-      }
-      websock.onerror = function(evt) {
-        console.log(evt);
-      }
-      websock.onmessage = function(evt) {
-          //console.log(evt); 
-          console.log(evt.data);
-          document.getElementById("serialConsole").innerHTML=Math.round(evt.data*calibrationFactor);
-          }
-            //parse_incoming_websocket_messages(evt.data);
-        }
-  
-  function doClose() {
-      //detach steering servo
-        //attachordetachservos("w","d");
-        
-        websock.close();
-  }
-  function doSend(message) {
-    if (iswebsocketconnected == true) {
-      if (websock.readyState == websock.OPEN) {
-        websock.send(message);
-          //console.log(message);
-      } else {
-        
-        console.log("websocket is in an indeterminate state");
-      }
-    }
-  }
-  </script>
-</html>
-
-
-)rawliteral";
-
-
-void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
-{
-//Serial1.printf("webSocketEvent(%d, %d, ...)\r\n", num, type);
-switch(type) {
-case WStype_DISCONNECTED:
-//Serial1.printf("[%u] Disconnected!\r\n", num);
-break;
-case WStype_CONNECTED:
-{
-IPAddress ip = webSocket.remoteIP(num);
-//Serial1.printf("[%u] Connected from %d.%d.%d.%d url: %s\r\n", num, ip[0], ip[1], ip[2], ip[3], payload);
-//itoa( cm, str, 10 );
-//  webSocket.sendTXT(num, str, strlen(str));
-}
-break;
-case WStype_TEXT:
-{
-//Serial1.printf("[%u] get Text: %s\r\n", num, payload);
-//Send whatever comes on the WS to Atmega.
-char *mystring1 = (char *)payload;
-mystring = mystring1;
-if (strcmp(mystring1,"tare") == 0)
-{
-  scale.tare();
-  }
-
-}
-break;
-case WStype_BIN:
-//Serial1.printf("[%u] get binary length: %u\r\n", num, length);
-break;
-default:
-//Serial1.printf("Invalid WStype [%d]\r\n", type);
-break;
-}
-}
-
-void handleRoot()
-{
-server.sendHeader("Cache-Control","max-age=604800");
-server.send(200, "text/html", INDEX_HTML);
-
-}
-void handleIndex()
-{
-//server.send_P(200, "text/html", INDEX_HTML);
-server.sendHeader("Cache-Control","max-age=604800");
-server.send(200, "text/html", INDEX_HTML);
 
 }
 
 
-void handleManifest()
-{
-server.sendHeader("Cache-Control","max-age=604800");
-server.send(200, "application/json", MANIFEST_JSON);
+Serial.println("WiFi Connected");
+}
+void senddata(){
 
-}
-void handleCheck()
-{
-server.sendHeader("Cache-Control","max-age=604800");
-server.send(200, "text/html", CHECK_SENSORS);
+if (!client.connect(host, port)) {
 
+return;
+}
+char str[32];
+//ADC*(1.1/1024) will give the Vout at the voltage divider
+//V=(Vout*((R1+R2)/R2))*1000 miliVolts
+batteryVoltage = ((analogRead(A0)*(1.1/1024))*((R1+R2)/R2))*1000;
+// convert the time into a distance
+
+if (scale.is_ready()) {
+
+
+float reading = scale.get_units(10); 
+
+Serial.println(reading);
+
+dtostrf(reading,10, 4,str);
+//webSocket.broadcastTXT(datastr, strlen(datastr));
 }
 
-void handleNotFound()
-{
-String message = "File Not Found\n\n";
-message += "URI: ";
-message += server.uri();
-message += "\nMethod: ";
-message += (server.method() == HTTP_GET)?"GET":"POST";
-message += "\nArguments: ";
-message += server.args();
-message += "\n";
-for (uint8_t i=0; i<server.args(); i++){
-message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+int alength = strlen(str);
+str[alength]=':';
+//Add the vcc value after ':'
+itoa( batteryVoltage, str+alength+1, 10 ); // for some reason +1 outputs starnge 2:32:283:26 or  2:11305:275:26
+
+
+
+Serial.println(str);
+client.print(str);
+
+Serial.println("sent");
+
+delay(5);
+//Close the socket - server is closing after one receive at the moment so it may not be necessary to close by the client
+client.stop();
+// Serial.println(millis()-currentmillis); //=1489
 }
-server.send(404, "text/plain", message);
-}
+
 
 
 
@@ -358,133 +130,24 @@ void setup()   {
 Serial.begin(9600);
 scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 //scale.set_scale(211.8*10.7);                      // this value is obtained by calibrating the scale with known weights; see above and the https://github.com/bogde/HX711 README for details
+scale.power_up();
+delay(20);
 scale.set_scale(226.626); 
-scale.tare(); 
+//scale.tare(); if tare after eac wake up - will always output zero
 
 Serial.println("All good in setup");
-
-/***************** AP mode*******************/
-
-WiFi.softAP(ssid, password);
-WiFi.printDiag(Serial);
-IPAddress myIP = WiFi.softAPIP();
-
-
-/***********************************************/
-
-/*****************Client Mode******************/
-/*
-WiFiMulti.addAP(ssid, password);
-while(WiFiMulti.run() != WL_CONNECTED) {
-Serial1.print(".");
+connect();
 delay(100);
-}
-Serial1.println("");
-Serial1.print("Connected to ");
-Serial1.println(ssid);
-Serial1.print("IP address: ");
-Serial1.println(WiFi.localIP());
-*/
-/******Web Updater via browser*****/
-httpUpdater.setup(&server);
-Serial.print("HTTPUpdateServer ready! Open http://");
-Serial.print(myIP);
-Serial.println("/update in your browser");
-/**** end web updater************/
-/**********************************************/
+senddata();
+delay(50); 
+scale.power_down();
+delay(20);
 
-server.on("/", handleRoot);
-server.on("/index.html",handleIndex);
-server.on("/manifest.json",handleManifest);
-server.on("/check.html",handleCheck);
-server.onNotFound(handleNotFound);
-//  server.onNotFound([]() {                              // If the client requests any URI
-//    if (!handleFileRead(server.uri()))                  // send it if it exists
-//      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
-//  });
-
-server.begin();
-
-webSocket.begin();
-webSocket.onEvent(webSocketEvent);
-
-
-/* ************SPIFFS********************* */
-//  if (SPIFFS.begin()){Serial1.println("file system mounted");};
-//
-//  //Open the "Save.txt" file and check if we were saving before the reset happened
-//  File q = SPIFFS.open("/Save.txt", "r");
-//  if (q.find("Y")){saveData=true;}
-//  q.close();
-
-/*********************************************/
-
-/* ************OTA********************* */
-
-// Port defaults to 8266
-// ArduinoOTA.setPort(8266);
-
-// Hostname defaults to esp8266-[ChipID]
-// ArduinoOTA.setHostname("myesp8266");
-
-// No authentication by default
-// ArduinoOTA.setPassword((const char *)"123");
-
-ArduinoOTA.onStart([]() {
-//Serial1.println("Start");
-});
-ArduinoOTA.onEnd([]() {
-//Serial1.println("\nEnd");
-});
-ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-//Serial1.printf("Progress: %u%%\r", (progress / (total / 100)));
-});
-ArduinoOTA.onError([](ota_error_t error) {
-/*
-Serial1.printf("Error[%u]: ", error);
-if (error == OTA_AUTH_ERROR) Serial1.println("Auth Failed");
-else if (error == OTA_BEGIN_ERROR) Serial1.println("Begin Failed");
-else if (error == OTA_CONNECT_ERROR) Serial1.println("Connect Failed");
-else if (error == OTA_RECEIVE_ERROR) Serial1.println("Receive Failed");
-else if (error == OTA_END_ERROR) Serial1.println("End Failed");
-*/
-});
-ArduinoOTA.begin();
-
-/****************************************************/  
-
+ESP.deepSleep(30e6);
 }
 
 
 void loop() {
 
-webSocket.loop();
-server.handleClient();
-ArduinoOTA.handle();
 
-unsigned long currentMillis = millis();
-
-if (currentMillis - previousMillis >= timeoutinterval) {
-previousMillis = currentMillis;
-
-if (scale.is_ready()) {
-
-
-float reading = (scale.get_units(10))*calibrationFactor; //With scale.set_scale(226.626) - this gives accurate up to 1 deca gram (10 grams). 500 gram water bottle values are 50.07 to 50.32
-//float readinginGs = reading*10;
-////float readinginKGs = round(readinginGs/1000); 
-//float readinginLBs = readinginGs/453.592;// 4545
-////Serial.print("reading in lbs= ");
-////Serial.println(readinginLBs);
-//
-//int intermediatevalueholder = (int) (round(readinginGs/100)); //70992.87 becomes  710
-//int leftofdecimal = intermediatevalueholder/10; //71
-////Serial.println(leftofdecimal);
-//int rightofdecimal = (intermediatevalueholder)-(leftofdecimal*10);
-Serial.println(reading);
-char datastr[16];
-dtostrf(reading,10, 4, datastr);
-webSocket.broadcastTXT(datastr, strlen(datastr));
-}
-}
 }
---
+++ ESP-Server/ESP-Server.ino
@@ -0,0 +1,440 @@
+/*
+19:33:54.810 -> Flash real id:   00144020
+19:33:54.810 -> Flash real size: 1048576 bytes
+19:33:54.810 -> 
+19:33:54.810 -> Flash ide  size: 1048576 bytes
+19:33:54.810 -> Flash ide speed: 40000000 Hz
+19:33:54.810 -> Flash ide mode:  DIO
+19:33:54.846 -> Flash Chip configuration ok
+
+SPIFFS 31 char filename limit
+
+struct FSInfo {
+    size_t totalBytes;
+    size_t usedBytes;
+    size_t blockSize;
+    size_t pageSize;
+    size_t maxOpenFiles;
+    size_t maxPathLength;
+};
+
+Keep track of available flash size. 
+Ability to find/connect/register clients - give it names. 
+Client wakes up - reads sensor data  - sleeps with putting HX711 in sleep mode as well. 
+Sort out tare, what to store, taring/zeroing etc.
+
+*/
+
+#include <ESP8266WiFi.h>
+//#include <ESP8266WiFiMulti.h>
+#include <WebSocketsServer.h>
+#include <Hash.h>
+#include <ESP8266WebServer.h>
+//#include <ESP8266mDNS.h>
+#include "FS.h"
+#include <ArduinoOTA.h>
+#include <string.h>
+
+#define MAX_STRING_LEN  32
+struct station_info *stat_info;
+IPAddress address;
+
+WiFiEventHandler stationConnectedHandler;
+WiFiEventHandler stationDisconnectedHandler;
+
+// Function to return a substring defined by a delimiter at an index
+char* subStr (char* str, char *delim, int index) {
+  char *act, *sub, *ptr;
+  static char copy[MAX_STRING_LEN];
+  int i;
+
+  // Since strtok consumes the first arg, make a copy
+  strcpy(copy, str);
+
+  for (i = 1, act = copy; i <= index; i++, act = NULL) {
+     //Serial.print(".");
+     sub = strtok_r(act, delim, &ptr);
+     if (sub == NULL) break;
+  }
+  return sub;
+
+}
+//char *record1 = "one two three";
+//char *record2 = "Hello there friend";
+//Serial.println(subStr(record1, " ", 1));
+//output = one
+
+
+
+const char *ssid = "ESP";
+const char *password = "monte123";
+
+
+
+ESP8266WebServer server(80);
+WiFiServer tcpserver(9999);//TCPserver
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+boolean saveData = false;
+boolean broadcast = false;
+int trigPin = 4;    
+int echoPin = 5;    
+long duration, serverHeight; 
+int clientHeight,ZeroOffset;
+char str[8];
+unsigned long currESPSecs, currTime,timestamp, zeroTime;
+
+static const char PROGMEM INDEX_HTML[] = R"rawliteral(
+)rawliteral";
+
+
+
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length)
+{
+  Serial.printf("webSocketEvent(%d, %d, ...)\r\n", num, type);
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[%u] Disconnected!\r\n", num);
+      break;
+    case WStype_CONNECTED:
+      {
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\r\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+      //itoa( cm, str, 10 );
+      //  webSocket.sendTXT(num, str, strlen(str));
+      }
+      break;
+    case WStype_TEXT:
+    {
+      //Serial.printf("[%u] get Text: %s\r\n", num, payload);
+
+      //Payload will be in the form of 3 alphapets and 3 digits
+      //DATA-105 means it is Data and value is 105
+      //COMMAND-ZERO means Command ZERO
+      //TIME-12345678 means epoch timestamp in seconds
+      
+      char *split1,*split2 ;
+     char *mystring = (char *)payload;
+      split1=subStr(mystring, "-", 1);
+      split2=subStr(mystring, "-", 2);
+      
+      if (strcmp(split1,"COMMAND") == 0)
+        {
+          //Serial.println("Received Command");
+          //Serial.println(split2);
+          if (strcmp(split2,"ZERO") == 0){
+           // Serial.println("Zero command received");
+            if (!clientHeight){
+              Serial.println("No client data");
+              } else {
+            ZeroOffset=serverHeight-clientHeight;
+            zeroTime=timestamp+((millis()/1000)-currESPSecs);
+            //Serial.println(currTime);
+            File f = SPIFFS.open("/Zero.txt", "w");
+            f.print("Time:");
+            f.print(zeroTime);
+            f.print("\n");
+            f.print("ZeroOffset:");
+            f.print(ZeroOffset);
+            f.print("\n");
+            f.close();
+            //Reset the currESPsecs to current millis at the zerotime
+            //So the data save time will be zero time + secs elapsed since zero time
+            currESPSecs = millis()/1000;
+            
+            }
+          } else if (strcmp(split2,"STARTSAVE") == 0){
+            
+            if (!saveData){
+              saveData=true;
+            File f = SPIFFS.open("/Data.txt", "w");
+            f.close();
+            //Create a file which keeps log of current saving process
+            File s = SPIFFS.open("/Save.txt", "w");
+            s.print("Y");
+            s.close();
+            }
+            
+            } 
+
+            else if (strcmp(split2,"STOPSAVE") == 0) 
+            {
+              if (saveData){saveData=false;
+                File s = SPIFFS.open("/Save.txt", "w");
+                s.print("N");
+                s.close();
+              }
+              }
+
+             
+            else if (strcmp(split2,"RUSAVING") == 0) 
+            {
+              if (saveData){
+                webSocket.sendTXT(num, "Y");
+                } else {
+                  
+                webSocket.sendTXT(num, "N");  
+                  }
+              }
+
+        
+        } 
+      else if (strcmp(split1,"DATA") == 0)
+        {
+          //Serial.println("Received Data");
+          //Serial.println(split2);
+          clientHeight=atoi(split2);
+          if (saveData){
+          //currTime=timestamp+((millis()/1000)-currESPSecs);
+          currTime=((millis()/1000)-currESPSecs);
+          File f = SPIFFS.open("/Data.txt", "a");
+          f.print(currTime);
+          f.print(":");
+          f.print("C:");
+          f.print(clientHeight);
+          f.print("\n");
+          f.close();
+          }
+          if(broadcast){
+           webSocket.broadcastTXT(split2, strlen(split2));
+            } else {
+              //If not broadcast - send to browser client num =1
+              webSocket.sendTXT(1,split2, strlen(split2));
+              
+              }
+          
+        } 
+      else if (strcmp(split1,"TIME") == 0)
+        {
+          //Serial.println("Received TimeStamp");
+          //Serial.println(split2);
+          timestamp=atol(split2);
+          //currTime=split2;
+          currESPSecs = millis()/1000;
+          //Serial.println(timestamp);
+          //Serial.println(currESPSecs);
+        }
+      else 
+      {
+          Serial.printf("Unknown-");
+          Serial.printf("[%u] get Text: %s\r\n", num, payload);
+          // send data to all connected clients
+          //webSocket.broadcastTXT(payload, length);
+      }
+    }
+    // clientHeight=atoi((const char *)payload);
+     // Serial.println((const char *)payload);
+     // itoa( cm, str, 10 );
+      // webSocket.sendTXT(0, str, strlen(str));
+      break;
+    case WStype_BIN:
+      Serial.printf("[%u] get binary length: %u\r\n", num, length);
+      hexdump(payload, length);
+
+      // echo data back to browser
+      webSocket.sendBIN(num, payload, length);
+      break;
+    default:
+      Serial.printf("Invalid WStype [%d]\r\n", type);
+      break;
+  }
+}
+
+void handleRoot()
+{
+  server.send_P(200, "text/html", INDEX_HTML);
+}
+
+
+
+
+//String getContentType(String filename); // convert the file extension to the MIME type
+bool handleFileRead(String path);       // send the right file to the client (if it exists)
+//String getContentType(String filename) { // convert the file extension to the MIME type
+//  if (filename.endsWith(".html")) return "text/html";
+//  else if (filename.endsWith(".css")) return "text/css";
+//  else if (filename.endsWith(".js")) return "application/javascript";
+//  else if (filename.endsWith(".ico")) return "image/x-icon";
+//  return "text/plain";
+//}
+
+bool handleFileRead(String path) { // send the right file to the client (if it exists)
+  //Serial.println("handleFileRead: " + path);
+  if (path.endsWith("/")) path += "index.html";         // If a folder is requested, send the index file
+  //String contentType = getContentType(path);            // Get the MIME type
+  if (SPIFFS.exists(path)) {                            // If the file exists
+    File file = SPIFFS.open(path, "r");                 // Open it
+    //size_t sent = server.streamFile(file, contentType); // And send it to the client
+    size_t sent = server.streamFile(file, "text/plain"); // And send it to the client
+    file.close();                                       // Then close the file again
+    return true;
+  }
+  //Serial.println("\tFile Not Found");
+  return false;                                         // If the file doesn't exist, return false
+}
+
+int fractionoccupiedspaceonfilesystem(){
+FSInfo fs_info;
+SPIFFS.info(fs_info);
+return fs_info.usedBytes/fs_info.totalBytes; 
+  
+  }
+
+
+void onStationConnected(const WiFiEventSoftAPModeStationConnected& evt) {
+  Serial.print("Station connected: ");
+  Serial.println(macToString(evt.mac));
+}
+
+void onStationDisconnected(const WiFiEventSoftAPModeStationDisconnected& evt) {
+  Serial.print("Station disconnected: ");
+  Serial.println(macToString(evt.mac));
+}
+String macToString(const unsigned char* mac) {
+  char buf[20];
+  snprintf(buf, sizeof(buf), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  return String(buf);
+}
+
+void setup()
+{ 
+
+
+  Serial.begin(115200);
+  delay(10);
+  //Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for(uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] BOOT WAIT %d...\r\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  Serial.print("Configuring access point...");
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+ Serial.println(myIP);
+
+// Call "onStationConnected" each time a station connects
+  stationConnectedHandler = WiFi.onSoftAPModeStationConnected(&onStationConnected);
+  // Call "onStationDisconnected" each time a station disconnects
+  stationDisconnectedHandler = WiFi.onSoftAPModeStationDisconnected(&onStationDisconnected);
+  
+  
+  server.on("/", handleRoot);
+  //server.onNotFound(handleNotFound);
+  server.onNotFound([]() {                              // If the client requests any URI
+    if (!handleFileRead(server.uri()))                  // send it if it exists
+      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
+
+  server.begin();
+ tcpserver.begin();
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
+
+
+
+  if (SPIFFS.begin()){Serial.println("file system mounted");};
+
+  //Open the "Save.txt" file and check if we were saving before the reset happened
+  File q = SPIFFS.open("/Save.txt", "r");
+  if (q.find("Y")){saveData=true;}
+  q.close();
+  
+  
+  
+  /* ************OTA********************* */
+
+// Port defaults to 8266
+// ArduinoOTA.setPort(8266);
+
+// Hostname defaults to esp8266-[ChipID]
+// ArduinoOTA.setHostname("myesp8266");
+
+// No authentication by default
+// ArduinoOTA.setPassword((const char *)"123");
+
+ArduinoOTA.onStart([]() {
+//Serial1.println("Start");
+});
+ArduinoOTA.onEnd([]() {
+//Serial1.println("\nEnd");
+});
+ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+//Serial1.printf("Progress: %u%%\r", (progress / (total / 100)));
+});
+ArduinoOTA.onError([](ota_error_t error) {
+/*
+Serial1.printf("Error[%u]: ", error);
+if (error == OTA_AUTH_ERROR) Serial1.println("Auth Failed");
+else if (error == OTA_BEGIN_ERROR) Serial1.println("Begin Failed");
+else if (error == OTA_CONNECT_ERROR) Serial1.println("Connect Failed");
+else if (error == OTA_RECEIVE_ERROR) Serial1.println("Receive Failed");
+else if (error == OTA_END_ERROR) Serial1.println("End Failed");
+*/
+});
+ArduinoOTA.begin();
+
+/****************************************************/  
+  
+  
+  }
+
+/*******TCP Server*****/
+void handletcpclient(){
+  String line;
+  WiFiClient client = tcpserver.available();
+  // wait for a client (web browser) to connect
+  if (client)
+  {
+    Serial.println("\n[Client connected]");
+    stat_info = wifi_softap_get_station_info();
+    Serial.print("Client IP Address = ");
+    Serial.print(address);
+    Serial.println();
+    Serial.print("Client MAC Address = ");
+    Serial.print(stat_info->bssid[0],HEX);
+    while (client.connected())
+    {
+      // read line by line what the client (web browser) is requesting
+      if (client.available())
+      {
+        line = client.readStringUntil('\0');
+        Serial.print(line);
+        
+      }
+    }
+
+//    while (client.available()) {
+//      // but first, let client finish its request
+//      // that's diplomatic compliance to protocols
+//      // (and otherwise some clients may complain, like curl)
+//      // (that is an example, prefer using a proper webserver library)
+//      client.read();
+//    }
+
+    // close the connection:
+    client.stop();
+    Serial.println("[Client disconnected]");
+  }
+  //webSocket.broadcastTXT(line, line.length());
+}
+/*****************************/
+
+void loop()
+{
+  webSocket.loop();
+  server.handleClient();
+  handletcpclient();
+
+}
